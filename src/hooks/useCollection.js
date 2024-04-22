f927e29ca9c2129aeac044b6e0720856cab43c34
unwantet <darkblezer59@gmail.com>
2024-04-23 00:16:36 +0500

    ref

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -5,26 +5,27 @@ import { db } from "../firebase/firebaseConfig";
 
 function UseCollection() {
     const [data, setData] = useState(null)
+    const [isPending , setIsPending] = useState(true);
     useEffect(() => {
 
         const getCollectionData = async () => {
             const querySnapshot = await getDocs(collection(db, "retseplar"));
             const allData = [];
-        querySnapshot.forEach((doc) => {
-
-        allData.push({
+            querySnapshot.forEach((doc) => {
+            allData.push({
             id: doc.id , ...doc.data()
-        })
-        
-        
-    });
+            })});
+            
     setData(allData)
+    if(allData.length > 0) {
+        setIsPending(false)
+    }
     }
     getCollectionData()
     },[])    
 
 
-    return {data}
+    return {data , isPending}
 }
 
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import AllRetsept from "../components/AllRetsept"
-import { useFetch } from "../hooks/useFetch"
 
 import { UseCollection } from "../hooks/useCollection"
 
 
 export default function Home() {
-  const {data : retsepts} = UseCollection();
-  console.log(retsepts);
+  const {data : retsepts , isPending} = UseCollection();
 
+  console.log(retsepts)
+
+  console.log(isPending)
+
+  if(isPending) return <div className="flex justify-center items-center h-screen">
+                          <span className="loading loading-bars w-24 h-24"></span>
+                        </div>
     return (
         <div className="max-w-screen-lg w-full mx-auto px-3">
         {retsepts && <AllRetsept retsepts={ retsepts } />}
